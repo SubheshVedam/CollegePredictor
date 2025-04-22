64bfa3ee0ad7e60561e718368c4be8c5789a451a
SubheshVedam <subhesh@vedam.org>
2025-04-22 16:19:01 +0530

    Subhesh_adding_Analytics

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -47,6 +47,11 @@
 /******/ 	__webpack_require__.m = __webpack_modules__;
 /******/ 	
 /************************************************************************/
+/******/ 	/* webpack/runtime/amd options */
+/******/ 	(() => {
+/******/ 		__webpack_require__.amdO = {};
+/******/ 	})();
+/******/ 	
 /******/ 	/* webpack/runtime/compat get default export */
 /******/ 	(() => {
 /******/ 		// getDefaultExport function for compatibility with non-harmony modules
@@ -125,7 +130,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("1bc6a288219913b4")
+/******/ 		__webpack_require__.h = () => ("fbfd1428733693c1")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
--- app/layout.js
+++ app/layout.js
@@ -1,18 +1,21 @@
 "use client";
+
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { AppRouterCacheProvider } from "@mui/material-nextjs/v15-appRouter";
-import { Poppins } from "next/font/google"; // Changed from Roboto to Poppins
+import { Poppins } from "next/font/google";
 import { ThemeProvider } from "@mui/material/styles";
 import theme from "../theme";
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
+import Script from "next/script"; // ✅ import Script from next/script
+import { Suspense } from "react";
 
 const poppins = Poppins({
-  weight: ["300", "400", "500", "600", "700"], // Poppins has different weights available
+  weight: ["300", "400", "500", "600", "700"],
   subsets: ["latin"],
   display: "swap",
-  variable: "--font-poppins", // Changed variable name
+  variable: "--font-poppins",
 });
 
 const geistSans = Geist({
@@ -31,6 +34,23 @@ export default function RootLayout({ children }) {
       <html lang="en" className={poppins.variable}>
         <head>
           <link rel="icon" href="./favicon.ico" />
+          <Suspense fallback={null}>
+            {/* <GoogleAnalytics /> */}
+          </Suspense>
+
+          {/* ✅ Load the gtag.js script using next/script */}
+          <Script
+            src="https://www.googletagmanager.com/gtag/js?id=G-EJQV1XVPZC"
+            strategy="afterInteractive"
+          />
+          <Script id="google-analytics" strategy="afterInteractive">
+            {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-EJQV1XVPZC');
+            `}
+          </Script>
         </head>
         <body
           className={`${poppins.className} ${geistSans.variable} ${geistMono.variable}`}
